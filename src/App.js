32a4b546f61d6f99843e0db9ef02ca50daa4395c
foderking <freatshow@gmail.com>
2021-07-30 13:20:52 -0700

    first look

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from 'react'
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism.css"; //Example style, you can use another
 import beaut from 'js-beautify'
 
 import AceEditor from "react-ace";
@@ -25,21 +20,9 @@ const App = () =>
 
   return (
     <div className='container'>
-      <h1>Deminifier.js</h1>
+      <h1 className='text-center'>Deminifier.js</h1>
 
-      {/* <div className='bg-light form-floating' >
-        <Editor
-          value={code}
-          onValueChange={(code) => setCode(code)}
-          highlight={(code) => highlight(code, languages.js)}
-          padding={10}
-          style={{
-            fontFamily: '"Fira code", "Fira Mono", monospace',
-            fontSize: 12,
-          }}
-        />
-      </div> */}
-      <div>
+      <div className='py-5'>
         <AceEditor
           placeholder="Paste code here!"
           mode="javascript"
